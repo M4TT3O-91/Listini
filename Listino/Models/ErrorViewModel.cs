e75fb17fd85f58dd80335f8afeb160f7d657e9f1
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-19 14:33:10 +0200

    new initial commit

--- Listino/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Listino.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Simple_Ecommerce/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Listino.Models
+{
+
+    public class Product
+    {
+        [Required]
+        [Key]
+        public string C8 { get; set; }
+        public string Descrizione { get; set; }
+        public string Titolo { get; set; }
+        public string Link { get; set; }
+
+    }
+}
